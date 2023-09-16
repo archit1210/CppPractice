906612d745c793526233ea3413c3eba1b0d05f69
archit1210 <architlokhande72@gmail.com>
2023-09-17 00:37:52 +0530

    #4

---
+++ Cube.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int area = 0;
+	int volume = 0;
+	int a;
+	cin>>a;
+	
+	area = 6*a*a;
+	volume = a*a*a;
+	
+	cout<<area<<" "<<volume;
+	return 0;
+}
